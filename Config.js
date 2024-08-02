db217477b74f58b62a87c009d746d58444643fe9
whatsappxyz <whatsappxyz6@gmail.com>
2024-08-02 10:27:14 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "916260273863"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘SHUBHAM𓆪_✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
@@ -12,9 +12,9 @@ global.location = "Indian"
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "💙𓆩‎✘SHUBHAM𓆪_✰",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "💙𓆩‎✘SHUBHAM𓆪_✰",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -27,13 +27,13 @@ global.prefa = process.env.PREFIX || ".",
 
 //false=disable and true=enable
 global.welcome = process.env.WELCOME || "false", //auto welcome
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
-global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autorecordtype = true //auto typing + recording
+global.autoread = true //auto read messages
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
@@ -53,9 +53,9 @@ global.mess = {
 module.exports = {
 ownernumber: global.ownernumber,
 ownername: global.ownername,
-sessionId: process.env.id || " ", // paste your session id here
+sessionId: process.env.id || "_M_A_R_I_A_U291bmRzRnJhbmNlCg==", // paste your session id here
 AUTO_BLOCK : process.env.PM_BLOCKER,
-STATUS_SAVER : "true",
+STATUS_SAVER : "false",
 ANTI_BOT:"true",
 }
 
